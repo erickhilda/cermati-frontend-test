08b81f9b862cd7356d03dd6119445608e564664c
Erick Hilda <erick.hilda@thebonza.com>
2021-11-26 09:32:36 +0700

    feat: add hero and notification component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,56 @@
+<script setup>
+import AppNavbar from "./AppNavbar.vue";
+</script>
+
+<template>
+  <div class="hero">
+    <AppNavbar />
+    <div class="mx-auto flex flex-row justify-center items-center mt-24">
+      <div class="text-white hero-text text-center">
+        <h1 class="title">Hello! I'm Erick Hilda</h1>
+        <h2 class="subtitle">Consult, Design, and Develop Websites</h2>
+        <p class="text-lg font-light">Have something great in mind? Feel free to contact me.</p>
+        <p class="text-xl font-light">I'll help you to make it happen.</p>
+        <button
+          class="bg-transparent hover:bg-white text-white hover:text-blue-700 font-bold py-2 px-4 mt-4 border border-white rounded-sm"
+        >Let's Make Contact</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.hero {
+  @apply bg-cover bg-center w-full h-screen bg-fixed;
+
+  background-image: -webkit-linear-gradient(
+      rgba(0, 74, 117, 0.7),
+      rgba(0, 74, 117, 0.7)
+    ),
+    url(../assets/work-desk__dustin-lee.jpg);
+  background-image: linear-gradient(
+      rgba(0, 74, 117, 0.7),
+      rgba(0, 74, 117, 0.7)
+    ),
+    url(../assets/work-desk__dustin-lee.jpg);
+}
+
+@media only screen and (max-width: 767px) {
+  .hero-text {
+    @apply mx-5;
+  }
+}
+
+.title {
+  @apply block font-normal tracking-wide text-4xl;
+
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+
+.subtitle {
+  @apply text-3xl font-medium pb-[15px];
+
+  word-spacing: 5px;
+}
+</style>
---
+++ src/components/AppNotification.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const isNotificationDisplay = ref(true);
+</script>
+
+<template>
+  <transition name="slide-fade">
+    <div class="sticky top-0 bg-[#e5e5e5]" v-if="isNotificationDisplay">
+      <div class="flex flex-row justify-center mx-auto p-4 notification-content">
+        <div class="max-w-[40rem]">
+          By accessing and using this website, you acknowledge that you have
+          read and understand our
+          <a
+            class="text-[#007bc1]"
+          >Cookie Policy</a>,
+          <a class="text-[#007bc1]">Privacy Policy</a>, and our
+          <a class="text-[#007bc1]">Terms of Service</a>.
+        </div>
+        <div>
+          <button
+            class="bg-[#007bc1] text-white font-light py-2 px-4 rounded-sm"
+            @click="isNotificationDisplay = !isNotificationDisplay"
+          >Got It!</button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped lang="postcss">
+.slide-fade-enter-active {
+  @apply transition ease-in-out duration-300;
+}
+.slide-fade-leave-active {
+  @apply transition ease-in-out duration-300;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateY(-10px);
+  opacity: 0;
+}
+
+@media only screen and (max-width: 767px) {
+  .notification-content {
+    @apply block;
+  }
+}
+</style>
