daa88d13df9e44f681e4e99095c37c19d82dc931
Erick Hilda <erick.hilda@thebonza.com>
2021-11-26 09:39:19 +0700

    chore: create card component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+
+const props = defineProps({
+  title: String,
+  cardIcon: String,
+  content: String,
+})
+</script>
+
+<template>
+  <div class="rounded-sm shadow-inner bg-[#e5e5e5] py-2 border-[#a9a9a9] border">
+    <div class="flex justify-between px-4 py-2">
+      <div>
+        <span class="font-normal text-[#2e2d33] text-lg">{{ title }}</span>
+      </div>
+      <Icon :icon="cardIcon" class="text-2xl text-[#a9a9a9]" />
+    </div>
+
+    <div class="px-4 py-2 text-left text-sm text-[#2e2d33]">{{ content }}</div>
+  </div>
+</template>
